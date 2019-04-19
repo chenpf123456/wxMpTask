30fc67a9ba6bb7fad53687e202051b9dd468fa5d
LX <398860254@qq.com>
2019-04-19 11:22:13 +0800

    扫描二维码

--- project/pages/scanCode/scanCode.js
+++ project/pages/scanCode/scanCode.js
@@ -47,7 +47,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
@@ -62,5 +62,39 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 扫码方法
+   */
+  scanCodeFun: function () {
+    wx.scanCode({
+      success: (res) => {
+        console.log(res);
+      }
+    });
+  },
+  data: {
+    qRCodeMsg: ''
+  },
+  getQRCode: function () {
+    var _this = this;
+
+    wx.scanCode({        //扫描API
+      success: function (res) {
+        console.log(res);    //输出回调信息
+        wx.showModal({
+          title: '链接',
+          content: res.result
+        });
+        _this.setData({
+          qRCodeMsg: res.result
+        });
+        // wx.showToast({
+        //   title: '成功',
+        //   duration: 2000
+        // })
+      }
+    })
   }
 })
