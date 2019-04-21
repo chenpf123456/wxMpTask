23a0d71b98b34961e0b452016dbcb0b97eccc403
Zhang Qiang <571695890@qq.com>
2019-04-21 20:33:12 +0800

    添加获取当前位置、手机电量、网络类型功能

--- project/pages/getLocation/getLocation.js
+++ project/pages/getLocation/getLocation.js
@@ -1,23 +1,46 @@
 // pages/getLocation/getLocation.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+Page({
+  onReady(e) {
+    // 使用 wx.createMapContext 获取 map 上下文
+    this.mapCtx = wx.createMapContext('myMap');
+  },
+  getMyCurrentLocation() {
+    this.mapCtx.moveToLocation();
+  },
+  //获取手机电量信息
+  getPhoneBatteryInfo() {
+    var chargeStr = '';
+    var _this = this;
+    wx.getBatteryInfo({
+      success(res) {
+        const isCharging = res.isCharging;
+        if (isCharging) {
+          chargeStr = '是';
+        } else {
+          chargeStr = '否';
+        }
+        _this.setData({
+          chargeStr: chargeStr
+        });
+        _this.setData({
+          level: res.level
+        });
+      }
+    });
 
   },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+  // 获取手机网络类型
+  getPhoneNetworkType() {
+    var _this = this;
+    var networkType = '';
+    wx.getNetworkType({
+      success(res) {
+        networkType = res.networkType;
+        _this.setData({
+          networkType: networkType
+        });
+      }
+    });
 
   }
 })
