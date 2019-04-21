1f43994b5e1f5f8851ed465436f3e204c61f2adb
Leo <a839766872@163.com>
2019-04-21 14:49:19 +0800

    增加手机号验证功能

--- project/pages/bindPhone/bindPhone.js
+++ project/pages/bindPhone/bindPhone.js
@@ -1,23 +1,91 @@
-// pages/bindPhone/bindPhone.js
-Component({
+Page({
   /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
-
+    ajxtrue: false,
   },
-
+  // 手机号验证
+  blurPhone: function (e) {
+    var phone = e.detail.value;
+    let that = this
+    if (!(/^1[34578]\d{9}$/.test(phone))) {
+      this.setData({
+        ajxtrue: false
+      })
+      if (phone.length >= 11) {
+        wx.showToast({
+          title: '手机号有误',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    } else {
+      this.setData({
+        ajxtrue: true
+      })
+      console.log('验证成功', that.data.ajxtrue)
+    }
+  },
+  // 表单提交
+  formSubmit(e) {
+    let that = this
+    let val = e.detail.value
+    let ajxtrue = this.data.ajxtrue
+    if (ajxtrue == true) {
+      wx.showModal({
+        title: '结果',
+        content: '验证成功',
+        complete :function(){
+          wx.navigateBack(); 
+        }
+      }) 
+    } else {
+      wx.showToast({
+        title: '手机号有误',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+  },
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载
    */
-  methods: {
-
+  onLoad: function (options) {
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
   }
 })
