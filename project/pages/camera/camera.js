91b4084f62c66ed9f4ce372406958e032646f289
wsyh0501 <1824613149@qq.com>
2019-04-22 17:05:12 +0800

    增加相机功能

--- project/pages/camera/camera.js
+++ project/pages/camera/camera.js
@@ -8,6 +8,40 @@ Page({
 
   },
 
+  openCamera: function () {
+    var that = this;
+    this.cameraCtx = wx.createCameraContext()
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], 
+      sourceType: ['camera'],
+      success: function (res) {
+        that.setData({
+          imagesrc: res.tempFilePaths
+        })
+      }
+    })
+  },
+  fail: function (res) {
+    console.log(res.errMsg)
+  },
+
+  startRecord: function () {
+    var that = this;
+    this.cameraCtx = wx.createCameraContext()
+    wx.chooseVideo({
+      sourceType: ['album', 'camera'],
+      maxDuration: 60,
+      camera: 'back',
+      success(res) {
+        that.setData({
+          videosrc: res.tempFilePath
+        })
+      }
+    })
+  },
+
+
   /**
    * 生命周期函数--监听页面加载
    */
